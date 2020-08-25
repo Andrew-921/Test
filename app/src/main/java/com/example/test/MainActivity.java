8d90ddf65f7b93e12b8b045d67af55b46590340a
Hui <blue012617@gmail.com>
2020-08-25 11:01:21 +0800

    new test

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        System.out.println("株株株");
+        System.out.println("000");
     }
 }
